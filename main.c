1d8d6f5cb85e9a2228f1a93410689a7c0d2ac4bb
Brianna Faith <faithbrianna01@gmail.com>
2023-10-17 21:02:26 +0300

    simple_shell

--- main.c
+++ main.c
@@ -1,46 +1,44 @@
 #include "shell.h"
 
-#define INFO_INIT { NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, 0, 1, 0, 0, 0, STDIN_FILENO, STDOUT_FILENO }
-
 /**
- * main- the entry point
- * @ac: argument count
- * @av: argument vector
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
  * Return: 0 on success, 1 on error
-*/
-
-int main (int ac, char **av)
+ */
+int main(int ac, char **av)
 {
-    info_t info = INFO_INIT;
-    int custom_fd = 2;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-    asm ("mov %1, %0\n\t"
-         "add $3, %0"
-         : "=r" (custom_fd)
-         : "r" (custom_fd));
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
 
-    if (ac == 2)
-    {
-        custom_fd = open(av[1], O_RDONLY);
-        if (custom_fd == -1)
-        {
-            if (errno == EACCES)
-                exit(126);
-            if (errno == ENOENT)
-            {
-                _eputs(av[0]);
-                _eputs(": 0: Can't open ");
-                _eputs(av[1]);
-                _eputchar('\n');
-                _eputchar(BUF_FLUSH);
-                exit(127);
-            }
-            return (EXIT_FAILURE);
-        }
-        info.readfd = custom_fd;
-    }
-    populate_env_list(&info);
-    read_history(&info);
-    hsh(&info, av);
-    return (EXIT_SUCCESS);
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(&info);
+	read_history(&info);
+	hsh(&info, av);
+	return (EXIT_SUCCESS);
 }
