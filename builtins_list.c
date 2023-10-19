596b3ccee27b0ce95e698ad05baaf7a080f26225
Brianna Faith <faithbrianna01@gmail.com>
2023-10-19 16:56:33 +0300

    simple_shell

--- builtins_list.c
+++ builtins_list.c
@@ -10,15 +10,16 @@ int builtins_list(data_of_program *data)
 {
 	int iterator;
 	builtins options[] = {
-		{"exit", builtin_exit},
 		{"help", builtin_help},
-		{"cd", builtin_cd},
-		{"alias", builtin_alias},
-		{"env", builtin_env},
-		{"setenv", builtin_set_env},
-		{"unsetenv", builtin_unset_env},
-		{NULL, NULL}
+		{"exit",builtin_exit},
+		{"cd",builtin_cd},
+		{"alias",builtin_alias},
+		{"env",builtin_env},
+		{"setenv",builtin_set_env},
+		{"unsetenv",builtin_unset_env},
+		{NULL,NULL}
 	};
+
 
 /*walk through the structure*/
 	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
