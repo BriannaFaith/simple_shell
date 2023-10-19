649cd4cbffbafbd69fe0c9bb3848d56010455a7c
Brianna Faith <faithbrianna01@gmail.com>
2023-10-19 19:21:05 +0300

    simple_shell

--- builtins_list.c
+++ builtins_list.c
@@ -1,26 +1,24 @@
 #include "shell.h"
-
 /**
- * builtins_list - search for match and execute the associate builtin
- * @data: struct for the program's data
+ * builtins_list - Searches for a match and execute the associate builtin
+ * @data: A struct for the program's data
  * Return: Returns the return of the function executed is there is a match,
  * otherwise returns -1.
  **/
+
 int builtins_list(data_of_program *data)
 {
 	int iterator;
 	builtins options[] = {
+		{"exit", builtin_exit},
 		{"help", builtin_help},
-		{"exit",builtin_exit},
-		{"cd",builtin_cd},
-		{"alias",builtin_alias},
-		{"env",builtin_env},
-		{"setenv",builtin_set_env},
-		{"unsetenv",builtin_unset_env},
-		{NULL,NULL}
+		{"cd", builtin_cd},
+		{"alias", builtin_alias},
+		{"env", builtin_env},
+		{"setenv", builtin_set_env},
+		{"unsetenv", builtin_unset_env},
+		{NULL, NULL}
 	};
-
-
 /*walk through the structure*/
 	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
 	{
